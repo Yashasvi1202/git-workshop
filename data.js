fd8e053c2674fa4c450a47073f3beff06be21b88
Yashasvi1202 <vanani.yashu25@gmail.com>
2025-11-01 12:00:20 +0530

    added new variable

--- data.js
+++ data.js
@@ -1,6 +1,9 @@
 let a=2;
 let b='2';
+let name="Yashasvi";
+let age=20;
 console.log(a==b);
 console.log(a == b);
-
-/* HELLO */
+console.log('my name is $(name)and age is$(age)');
+let newStr='my name is $(name)and age is$(age)';
+console.log("new string",newStr);
